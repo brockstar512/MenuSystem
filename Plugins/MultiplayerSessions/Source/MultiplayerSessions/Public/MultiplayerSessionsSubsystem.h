58bc1e311fa035e2f5cab1c7d2d7fe24d31bec94
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2023-06-07 11:45:00 -0500

    added encapsulated delegates

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -16,8 +16,44 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstan
 	
 	public:
 		UMultiplayerSessionsSubsystem();
+
+		//To Handle session functionality. The menu class will call this
+
+		void CreateSession(int32 NumPublicConnections, FString MatchType);
+		void FindSessions(int32 MaxSearchResults);
+		void JoinSession(const FOnlineSessionSearchResult& SessionResult);
+		void DestroySession();
+		void StartSession();
+
 	protected:
+		//internal callbacks for the delegates we'll add to the online session interface delegate list
+		//these sone need to be called outside of this class
+
+		void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+		void OnFindSessionsComplete(bool bWasSuccessful);
+		void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+		void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+		void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
+
+
+
 
 	private:
 		IOnlineSessionPtr SessionInterface;
+
+		//To add to the online session interface delegate list
+		//we'll bind our  Multiplayer subsystem internal callbacks to these.
+
+		FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+		FDelegateHandle CreateSessionCompleteDelegateHandle;
+		FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+		FDelegateHandle FindSessionCompleteDelegateHandle;
+		FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+		FDelegateHandle JoinSessionCompleteDelegateHandle;
+		FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+		FDelegateHandle DestroySessionCompleteDelegateHandle;
+		FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+		FDelegateHandle StartSessionCompleteDelegateHandle;
+
+
 };
